412bc1ca9e6b04f33f205232afee5beccf4922e2
GreenDragon <elf-github@arashi.com>
2009-05-14 11:57:30 -0700

    Pass two infected by gitmind

--- http_server/lib/http_server.rb
+++ http_server/lib/http_server.rb
@@ -4,44 +4,114 @@ require 'socket'
 
 class HttpServer
   VERSION = '1.0.0'
-  HTTP_VER='HTTP/0.9'
-  HTTP_PORT=8080
-  CRLF="\n\r\n\r"
 
-  attr_accessor :request, :header, :time
+  PROTOCOL_VERSION = 'HTTP/0.9'
+  PORT = 8080
   
-  def initialize
-    @request = ""
-    @header = ""
-    @basedir = FileUtils.pwd() + "/root"
+  METHODS = [ "GET" ]
+  
+  CRLF="\n\r"
+
+  class Response
+    attr_accessor :body, :redirect, :status, :headers
+
+    @@status = {
+      200 => "OK",
+      302 => "Found",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }
+
+    def initialize
+      @body = nil
+      @redirect = nil
+      @status = 200
+      @headers = {}
+    end
+
+    def content
+      q = ""
+      q << header_string(@status) << HttpServer::CRLF
+      q << "Date: #{Time.now.httpdate}" << HttpServer::CRLF
+      q << "Allow: #{HttpServer::METHODS.join(',')}" << HttpServer::CRLF
+
+      case @status
+      when 200
+        q << "Last-Modified: #{Time.now.httpdate}" << HttpServer::CRLF
+        if @body.nil? then
+          q << "Content-Length: 0" << HttpServer::CRLF
+        else
+          q << "Content-Length: #{@body.size}" << HttpServer::CRLF
+        end
+        q << "Content-Type: text/html" << HttpServer::CRLF
+        q << HttpServer::CRLF
+        q << @body unless @body.nil?
+      when 302
+        q << "Location: http://localhost:#{HttpServer::PORT}#{@redirect}" 
+        q << HttpServer::CRLF
+      when 404
+        q << HttpServer::CRLF
+        q << "Your file was so big." << HttpServer::CRLF
+        q << "It might be very useful." << HttpServer::CRLF
+        q << "But now it is gone." << HttpServer::CRLF
+      when 500
+        q << HttpServer::CRLF
+        q << "<p>Something <strong>Wicked</strong> This Way Went</p>"
+      else
+        raise "Unsupported Status Code #{@status}"
+      end
+      q
+    end
+  
+  private
+
+    def header_string(status)
+      "#{HttpServer::PROTOCOL_VERSION} #{status} #{@@status[status]}"
+    end
   end
 
+  class Request
+    attr_accessor :method, :path, :protocol, :headers
+
+    def initialize(request)
+      @headers = {}
+      parse(request)
+    end
+
+  private
+
+    def parse(request)
+      request.each_line do |line|
+        line.strip!
+        if line =~ /^(\w+) (\/.*) HTTP\/(\d+\.\d+)/ then
+          @method, @path, @protocol = $1, $2, $3
+        elsif line =~ /^(.+): (.+)/
+          @headers[$1] = $2
+        end
+      end
+    end
+  end
+
+  class Util
+    def prepare_request(string="/")
+      "GET #{string} " + HttpServer::PROTOCOL_VERSION + HttpServer::CRLF
+    end
+  end
+    
+  #def initialize
+  #  @header = ""
+  #  @basedir = FileUtils.pwd() + "/root"
+  #end
+
   def start
-    server = TCPServer.new('localhost',HTTP_PORT)
+    server = TCPServer.new('localhost',PORT)
     while session = server.accept
       session.write "Hello#{CRLF}"
       session.close
     end
   end
 
-  def prepare_request(string)
-    @request = "GET #{string} #{HTTP_VER}#{CRLF}"
-  end
-
-  def get(request)
-    prepare_request(request)
-  end
-
-  def build_header(size=0, status="200 OK", type="text/html", last_mod=Time.now.httpdate)
-    @header = "#{HTTP_VER} #{status}#{CRLF}" +
-    "Date: #{Time.now.httpdate}#{CRLF}" + 
-    "Last-Modified: #{last_mod}#{CRLF}" +
-    "Content-Length: #{size}#{CRLF}" +
-    "Content-Type: #{type}#{CRLF}" +
-    "#{CRLF}"
-  end
-
-  def debug
-    "BASEDIR " + @basedir
-  end
+  #def debug
+  #  "BASEDIR: " + @basedir
+  #end
 end
--- http_server/test/test_http_server.rb
+++ http_server/test/test_http_server.rb
@@ -22,44 +22,121 @@ end
 
 Time.testing_offset = 0
 
+# end snark
+
 class TestHttpServer < Test::Unit::TestCase
   def setup
-    @crlf = "\n\r\n\r"
-    @time = Time.now.httpdate
+    @crlf = "\n\r"
+    @now = Time.now.httpdate
 
+    @response = HttpServer::Response.new
     @server = HttpServer.new
+    @util = HttpServer::Util.new
+
     @threads = []
+
+    @raw = "GET / HTTP/0.9#{@crlf}" <<
+            "Host: localhost#{@crlf}" <<
+            "Date: #{@now}#{@crlf}#{@crlf}"
 
     # warn name
   end
 
   def test_prepare_request
-    @server.prepare_request("/index.html")
-
-    actual = @server.request
-    expected = "GET /index.html HTTP/0.9#{@crlf}"
+    actual = @util.prepare_request("/index.html")
+    expected = "GET /index.html HTTP/0.9" + @crlf
 
     assert_equal expected, actual
   end
 
-  def test_default_header
-    @server.build_header(0)
- 
-    actual = @server.header
-    expected = "HTTP/0.9 200 OK#{@crlf}" +
-    "Date: #{@time}#{@crlf}" +
-    "Last-Modified: #{@time}#{@crlf}" +
-    "Content-Length: 0#{@crlf}" +
-    "Content-Type: text/html#{@crlf}" +
-    "#{@crlf}"
+  def test_prepare_request_adds_default_path
+    actual = @util.prepare_request
+    expected = "GET / HTTP/0.9" + @crlf
 
     assert_equal expected, actual
   end
 
-  def test_http_server_debug_basedir
-    actual = @server.debug
-    assert_match(/^BASEDIR.*\/root$/, actual)
+  def test_parse_request
+    req = @util.prepare_request("/index.html")
+
+    @request = HttpServer::Request.new(req)
+
+    assert_equal "GET",           @request.method
+    assert_equal "/index.html",   @request.path
+    assert_equal "0.9",           @request.protocol
   end
+
+  def test_parse_request_headers
+    @request = HttpServer::Request.new(@raw)
+
+    assert_equal @request.headers, {
+      "Host" => "localhost",
+      "Date" => @now
+    }
+  end
+
+  def test_response_defaults
+    assert_nil @response.body
+    assert_equal 200, @response.status
+    assert_equal Hash.new, @response.headers
+  end
+
+  def test_response_header_200
+    actual = @response.content
+    expected =  "HTTP/0.9 200 OK" + @crlf +
+                "Date: #{@now}" + @crlf +
+                "Allow: GET" + @crlf +
+                "Last-Modified: #{@now}" + @crlf +
+                "Content-Length: 0" + @crlf +
+                "Content-Type: text/html" + @crlf +
+                "#{@crlf}"
+
+    assert_equal expected, actual
+  end
+
+  def test_response_header_404
+    @response.status = 404
+    
+    actual = @response.content
+    expected =  "HTTP/0.9 404 Not Found" + @crlf +
+                "Date: #{@now}" + @crlf +
+                "Allow: GET" + @crlf +
+                @crlf +
+                "Your file was so big." + @crlf +
+                "It might be very useful."  + @crlf +
+                "But now it is gone." + @crlf
+
+    assert_equal expected, actual
+  end
+
+  def test_response_header_500
+    @response.status = 500
+    
+    actual = @response.content
+    expected =  "HTTP/0.9 500 Internal Server Error" + @crlf +
+                "Date: #{@now}" + @crlf +
+                "Allow: GET" + @crlf +
+                @crlf +
+                "<p>Something <strong>Wicked</strong> This Way Went</p>"
+  end
+
+  def test_response_header_302
+    @response.status = 302
+    @response.redirect = "/redirected.erb"
+
+    actual = @response.content
+    expected =  "HTTP/0.9 302 Found" + @crlf +
+                "Date: #{@now}" + @crlf +
+                "Allow: GET" + @crlf +
+                "Location: http://localhost:8080/redirected.erb" + @crlf
+
+    assert_equal expected, actual
+  end
+
+  #def test_http_server_debug_basedir
+  #  actual = @server.debug
+  #  assert_match(/^BASEDIR.*\/root$/, actual)
+  #end
 
   def test_http_server_answers
     @threads << Thread.new do
